fc70a3c5a9c810b5fc6971aca1ac9c1e4976d4e0
AraamEshwarprasad <138553427+AraamEshwarprasad@users.noreply.github.com>
2023-07-05 16:30:23 +0530

    no issue
    commit

---
+++ project/src/test/java/TestCase/loginpgTC.java
@@ -0,0 +1,11 @@
+package TestCase;
+
+import pkg_json.loginpage;
+
+public class loginpgTC 
+{
+	public static void orangeHRMlogin_TC() throws InterruptedException
+	{
+		loginpage.loginpageactions();
+	}
+}
