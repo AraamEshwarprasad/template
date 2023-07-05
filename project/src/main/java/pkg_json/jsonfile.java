e365fb16821ffc76bce0b5ae34852f2f7248544a
AraamEshwarprasad <138553427+AraamEshwarprasad@users.noreply.github.com>
2023-07-05 15:51:03 +0530

    no issue
    commit

--- project/src/main/java/pkg_json/jsonfile.java
+++ project/src/main/java/pkg_json/jsonfile.java
@@ -1,5 +1,9 @@
 package pkg_json;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,11 +11,19 @@ public class jsonfile
 {
 
 	public static WebDriver driver;
+	
+	public static Properties p=new Properties();
+	
+	public static void Base() throws IOException
+	{
+		FileInputStream fis =new FileInputStream("D:\\JSON_WORKSPACE\\template\\project\\src\\test\\resources\\propertyfile");
+		p.load(fis);
+	}
 
 	public static void driverinstallation()
 	{
 		driver = new ChromeDriver();
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.get(p.getProperty("url"));
 		driver.manage().window().maximize();
 
 	}
